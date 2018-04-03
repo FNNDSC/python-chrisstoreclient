ed1cc077768bdf7460e0cf3715b2718bb78428ed
Jorge <jbernal0019@yahoo.es>
2018-04-03 11:04:33 -0400

    v0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'python-chrisstoreclient',
-      version          =   '0.0.1',
+      version          =   '0.0.2',
       description      =   '(Python) client for the ChRIS store API',
       long_description =   readme(),
       author           =   'FNNDSC',
