21d9a9b07a509e9d06f1e33069b9f2cd3f31383e
jbernal0019 <jbernal0019@yahoo.es>
2018-05-29 14:44:42 -0400

    v0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'python-chrisstoreclient',
-      version          =   '0.0.3',
+      version          =   '0.0.4',
       description      =   '(Python) client for the ChRIS store API',
       long_description =   readme(),
       author           =   'FNNDSC',
